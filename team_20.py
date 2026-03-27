606d8af36673bddd0ea8a8fb2ecfbae897215966
Pleșu Dinu <dinu.plesu@isa.utm.md>
2026-03-27 11:46:08 +0200

    Add TSFH function to handle opponent's history
    Implement TSFH strategy for opponent interaction

--- team_20.py
+++ team_20.py
@@ -1,1 +1,5 @@
-
+def TSFH(my_history, opponent_history)
+    #We do not want to cooperate first, we defend, so they know that we can defend, so they don't take advantaage of us thinking that we are 'fools' , who will only cooperate, we are oportunists, but more about instilling fear into the opponent
+    if len(opponent_history)==0:
+      return 'D'
+    return opponent_history[-1]
